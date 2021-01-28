a7977c6a2482b1fc4667547d22f7da65ac00b7ff
dari <hernandezdarifrancisco@gmail.com>
2021-01-28 02:28:35 -0600

    Send emails: one email to many recipients

--- send_email.py
+++ send_email.py
@@ -2,8 +2,15 @@
 
 import smtplib
 
+# List of recipents
+to_emails = [
+    "darialternative@yahoo.com",
+    "darialternative@outlook.com",
+    "darialternative@aol.com"
+]
+
 # Conectarse al servidor y puerto
-smtpObj = smtplib.SMTP ('smtp.gmail.com.mx', 587)
+smtpObj = smtplib.SMTP ('smtp.gmail.com', 587)
 
 # Send hello to smtp
 smtpObj.ehlo()
@@ -14,13 +21,16 @@ smtpObj.starttls()
 # Iniciar sesión
 smtpObj.login('darialternative@gmail.com', 'AliciaParadoxa1999x3')
 
-# Enviar correo
-smtpObj.sendmail('darialternative@gmail.com', 
-                    'darialternative@yahoo.com', 
-                    'Subject: Email example\n\nThis is an example email')
+# Loop for each email in list
+for email in to_emails: 
 
-# Confirmation message
-print ("Correo enviado")
+    # Enviar correo
+    smtpObj.sendmail('darialternative@gmail.com', 
+                        email, 
+                        'Subject: Email example\n\nThis is an example email')
+
+    # Confirmation message
+    print ("Correo enviado a {}".format (email))
 
 # Cerrar conexión
 smtpObj.quit()
